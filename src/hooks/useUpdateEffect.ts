3c8e4fbcb77e05af207645278c79a6e9855a5a2c
Pranay-Tej <s.pranayteja@gmail.com>
2022-01-02 11:22:21 +0530

    add useFirstRender,useUpdateEffect hooks

---
+++ src/hooks/useUpdateEffect.ts
@@ -0,0 +1,13 @@
+import { DependencyList, EffectCallback, useEffect } from "react";
+import useIsFirstRender from "./useIsFirstRender";
+
+// Same as useEffect but does not run on the first render.
+export default function useUpdateEffect(effect: EffectCallback, deps: DependencyList){
+  const isFirstRender = useIsFirstRender();
+
+  useEffect(() => {
+    if(!isFirstRender){
+      effect();
+    }
+  }, [deps]);
+}
